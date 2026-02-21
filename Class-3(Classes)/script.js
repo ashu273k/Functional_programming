cf98985c7d96e70fd4d8b1a55c5ccb8a687603d5
ashu273k <ashu9472.as@gmail.com>
2026-02-21 12:37:59 +0530

    Encapsulation using # in JS

--- Class-3(Classes)/script.js
+++ Class-3(Classes)/script.js
@@ -1,15 +1,32 @@
 // Classes
 
 class Pizza {
+
+    static totalPizza = 0
+    #size
+
     constructor(size, toppings, prefrence, crust) {
-        this.size = size;
+        this.#size = size;
         this.toppings = toppings;
         this.prefrence = prefrence;
         this.crust = crust;
+        Pizza.totalPizza++;
+    }
+
+    static showTotalPizza() {
+        console.log(`Total pizzas made: ${Pizza.totalPizza}`);
     }
 
     serve() {
-        console.log(`The ${this.size} is ready to be served!`);
+        console.log(`The ${this.#size} is ready to be served! (Normal method)`);
+    }
+
+    #describe() {
+        console.log('Tasty and juicy pizza!');
+    }
+
+    test() {
+        this.#describe();
     }
 
 }
@@ -17,12 +34,13 @@ class Pizza {
 let pizza1 = new Pizza('large', ['pepperoni', 'mushrooms'], 'veggie', 'thin');
 console.log(pizza1);
 pizza1.serve();
+pizza1.test();
 
 let pizza2 = new Pizza();
 // pizza2.serve();
 // This will throw an error because the properties of pizza2 are undefined and we are trying to access them in the serve method.
 
-
+Pizza.showTotalPizza();
 /**
  * Four paradigms of OOPs:
  * Inheritance
@@ -45,7 +63,7 @@ class StuffedPizza extends Pizza {
     }
 
     serve() {
-        console.log(`The ${this.size} ${this.prefrence} pizza with ${this.crust} crust and ${this.toppings.join(', ')} toppings is ready to be served! It has ${this.stuffing} stuffing.`);
+        console.log(`The ${this.size} ${this.prefrence} pizza with ${this.crust} crust and ${this.toppings.join(', ')} toppings is ready to be served! It has ${this.stuffing} stuffing.(Overridden method)`);
     }
 
 }
@@ -53,3 +71,7 @@ class StuffedPizza extends Pizza {
 let stuffedPizza1 = new StuffedPizza('medium', ['olives', 'onions'], 'veggie', 'thick', 'cheese');
 console.log(stuffedPizza1);
 stuffedPizza1.serve();
+
+// Encapsulation: Encapsulation is the process of hiding the internal details of an object and only exposing a public interface.
+// In modern JS to make private we use # before the property name.
+// In JS, we can achieve encapsulation using closures or by using the new private fields syntax.
