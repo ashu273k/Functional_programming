db222a776b2209cc5fa2329549d309434d7ee621
ashu273k <ashu9472.as@gmail.com>
2026-03-09 15:12:59 +0530

    Higher Order function myMap added!

---
+++ Class-5(Higher-Order Function)/MyMap.js
@@ -0,0 +1,44 @@
+// Map ->
+// callback
+// iteration
+// return the result in a new Array
+// This exists inside the Array Prototype
+
+// let arr = [1, 2, 3, 4]
+
+// let square = arr.map((num) => {
+//     return num*num
+// })
+
+// console.log(square)
+
+// Polyfill for Map
+
+Array.prototype.myMap = function(callback) {
+    // this -> working array
+
+    if (Array.isArray(this)) {
+
+        if(typeof callback != 'function') {
+            throw new Error('the function is required as the callback')
+        }
+        
+            let results = []
+        
+            for (let i = 0; i < this.length; i++) {
+                results.push(callback(this[i]))
+            }
+        
+            return results;
+    } else {
+        throw new Error('No array Found')
+    }
+
+}
+
+let nums1 = [1, 2, 3, 4, 5]
+let nums2 = [1, 2, 3, 4, 5, 6, 7]
+
+console.log(nums1.myMap(function(num) {
+    return num*num
+}))
